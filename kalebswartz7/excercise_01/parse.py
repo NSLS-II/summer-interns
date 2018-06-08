a2554275711eb61a64a9cf63fb1e185a55e155f7
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-08 10:45:46 -0400

    All changes requested implemented

--- kalebswartz7/excercise_01/parse.py
+++ kalebswartz7/excercise_01/parse.py
@@ -7,125 +7,165 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Just default values, these are changed based on the dat file
-horizontal = 1 
-vertical = 1
 
-#initialize the dictionary for storing parameters given in the dat file 
-initial_data = {'photon': {}, 'horizontal': {}, 'vertical': {}}
-matrix = [[]] 
+def get_parameters(d):
+    """
+    Read in first 10 lines of input file to get parameters for data 
 
-""" 
-Get data from the dat file with the parameters for the scan. Put this data into an organized Dictionary
-"""
-def get_parameters():
+    Parameters
+    ----------
+    d : dictionary
+        dictionary that is going to be populated by the parameter data
+    
+    Notes
+    -----
+
+    """
     with open('res_int_pr_se.dat') as file:
-        line_num = 1
-        for line in file:
-            if "#" not in line:
-                return
-            else:
-                if line_num == 1:
-                    populate_dictionary(line, '[', ']', 'intensity')
-                elif 'Initial Photon' in line:
-                    populate_dictionary(line, '#', ' ', 'photon', 'Initial_Energy')
-                elif 'Final Photon' in line:
-                    populate_dictionary(line, '#', ' ', 'photon', 'Final_Energy')
-                elif 'Initial Horizontal' in line:
-                    populate_dictionary(line, '#', ' ', 'horizontal', 'Initial_Position')
-                elif 'Final Horizontal' in line:
-                    populate_dictionary(line, '#', ' ', 'horizontal', 'Final_Position')
-                elif 'vs Horizontal' in line:
-                    populate_dictionary(line, '#', ' ', 'horizontal', 'Points', 'float','horizontal')
-                elif 'Initial Vertical' in line:
-                    populate_dictionary(line, '#', ' ', 'vertical', 'Initial_Position')
-                elif 'Final Vertical' in line:
-                    populate_dictionary(line, '#', ' ', 'vertical', 'Final_Position')
-                elif 'vs Vertical' in line:
-                    populate_dictionary(line, '#', ' ', 'vertical', 'Points', 'float','vertical')
+        for i in range(10):
+            line = file.readline()
+            if '/' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='[', ending_point=']', d='intensity', type1='string')
+            elif 'Initial Photon' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='photon', d1='Initial_Energy')
+            elif 'Final Photon' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='photon', d1='Final_Energy')
+            elif 'Initial Horizontal' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='Initial_Position')
+            elif 'Final Horizontal' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='Final_Position')
+            elif 'vs Horizontal' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='Points', type1='int')
+            elif 'Initial Vertical' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='Initial_Position')
+            elif 'Final Vertical' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='Final_Position')
+            elif 'vs Vertical' in line:
+                populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='Points', type1='int')
 
-            line_num += 1
 
-"""
-Funciton that populates the dictionary based on what values from the line are needed, what type they should be, and if they need to change 
-the values of any global varaiables 
-"""
+def populate_dictionary(line, d_to_populate, starting_point, ending_point, d, d1=None, type1='float'):
+    """
+    Populate dictionary based on wanting a certain portion of a string 
 
-def populate_dictionary(line, starting_point, ending_point, d, d1=None, type1='string', v_to_change=0):
-    change_h = False
-    change_v = False
+    Parameters
+    ----------
+    line : string
+        string that contains 'substring'
+    d_to_populate : dictionary
+        first level dictionary to store 'substring'
+    starting_point : string
+        string character that comes right before 'substring'
+    ending_point : string
+        string character that comes right after 'substring'
+    d : dictionary
+        second level dictionary to store 'substring' 
+    d1 : dictionary
+        third level dictionary to store 'substring', optional
+    type1 : string
+        what type 'substring' is when stored in dictionary
+    
+    Notes
+    -----
 
-    if v_to_change is 'horizontal':
-        change_h = True
-    elif v_to_change is 'vertical':
-        change_v = True
-
+    """
     start = line.find(starting_point) + 1
     end = line.find(ending_point)
 
     if type1 is 'string':
-        v_to_change = (line[start:end])
+        substring_value = (line[start:end])
+    elif type1 is 'int':
+        substring_value = int(line[start:end])
     else:
-        v_to_change = int(line[start:end])
+        substring_value = float(line[start:end])
 
     #Checks to see if d1 is actually necessary 
     if not d1:
-        initial_data[d] = v_to_change
+        d_to_populate[d] = substring_value
     else:
-        initial_data[d][d1] = v_to_change
-
-    #Checks to see if horizontal / vertical global values should be changed 
-    if change_h:
-        setHorizontal(v_to_change)
-    elif change_v:
-        setVertical(v_to_change)
+        d_to_populate[d][d1] = substring_value
 
 
-def setHorizontal(value):
-    global horizontal
-    horizontal = value
+def populate_matrix(matrix, horizontal, vertical):
+    """
+    Reads in all non-commented data from file, parses into 2D array based on Horizontal and Vertical Points 
 
-
-def setVertical(value):
-    global vertical
-    vertical = value
-
-
-def populate_matrix():
-    global matrix
+    Parameters
+    ----------
+    matrix : 2D array 
+        array that will be populated by the data parsed in the input file
+    horizontal : int 
+        # of horizontal rows in matrix
+    vertical : int
+        # of vertical rows in matrix
+    
+    Notes
+    -----
+    
+    """
     matrix = [[0 for i in range(horizontal)] for j in range(vertical)] 
     with open('res_int_pr_se.dat') as file:
         h, v = 0, 0
         for line in file:
-            if "#" not in line:
+            if '#' not in line:
                 matrix[v][h] = float(line)
                 if (h < horizontal - 1):
                     h += 1
                 else:
                     h = 0
                     v += 1
+    return matrix
 
 
-def populate_matrix_smart():
+def populate_matrix_smart(matrix, horizontal, vertical):
+    """
+    Reads in all non-commented data from file, parses into 2D array based on Horizontal and Vertical Points (using numpy) 
+
+    Parameters
+    ----------
+    matrix : 2D array 
+        array that will be populated by the data parsed in the input file
+    horizontal : int 
+        # of horizontal rows in matrix
+    vertical : int
+        # of vertical rows in matrix
+    
+    Notes
+    -----
+    
+    """
     d = np.loadtxt('res_int_pr_se.dat')
-    d2 = d.reshape((294, 528))
-    global matrix
+    d2 = d.reshape((vertical, horizontal))
     matrix = d2
+    return matrix
 
 
-def create_image(): 
-   im = plt.imshow(matrix, cmap='gray', aspect="equal")
-   plt.colorbar(im, orientation='vertical')
-   plt.show()
+def create_image(matrix): 
+    """
+    Creates 2D image based on data points in matrix
+    Parameters
+    ----------
+    matrix : 2D array 
+        array that contains data for image
+    
+    Notes
+    -----
+    
+    """
+    im = plt.imshow(matrix, cmap='gray', aspect='equal')
+    plt.colorbar(im, orientation='vertical')
+    plt.show()
 
 
-# np.loadtxt()
-# import pandas
 if __name__ == '__main__':
-    get_parameters()
+    initial_data = {'photon': {}, 'horizontal': {}, 'vertical': {},}
+    matrix = [[]] 
 
-    #populateMatrixSmart()
+    get_parameters(initial_data)
+    horizontal = initial_data['horizontal']['Points']
+    vertical = initial_data['vertical']['Points']
 
-    populate_matrix()
+    #matrix = populate_matrix_smart(matrix, horizontal, vertical)
+    matrix = populate_matrix(matrix, horizontal, vertical)
     print(initial_data)
-    create_image() 
+    create_image(matrix) 
