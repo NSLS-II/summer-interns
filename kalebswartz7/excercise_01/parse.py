de32a3cbaa80e72eb4a32877d7e565bf34c68cf9
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-11 14:03:33 -0400

    Progress - creating Pull Request to get some help

--- kalebswartz7/excercise_01/parse.py
+++ kalebswartz7/excercise_01/parse.py
@@ -6,6 +6,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.transforms as tf
+from matplotlib import gridspec
 
 
 def get_parameters(d):
@@ -30,16 +32,19 @@ def get_parameters(d):
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='photon', d1='initial_energy')
             elif 'Final Photon' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='photon', d1='final_energy')
+                populate_dictionary(line, d_to_populate = d, starting_point='[', ending_point=']', d='photon', d1='units', type1='string')
             elif 'Initial Horizontal' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='initial_position')
             elif 'Final Horizontal' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='final_position')
+                populate_dictionary(line, d_to_populate = d, starting_point='[', ending_point=']', d='horizontal', d1='units', type1='string')
             elif 'vs Horizontal' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='horizontal', d1='points', type1='int')
             elif 'Initial Vertical' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='initial_position')
             elif 'Final Vertical' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='final_position')
+                populate_dictionary(line, d_to_populate = d, starting_point='[', ending_point=']', d='vertical', d1='units', type1='string')
             elif 'vs Vertical' in line:
                 populate_dictionary(line, d_to_populate = d, starting_point='#', ending_point=' ', d='vertical', d1='points', type1='int')
 
@@ -143,27 +148,63 @@ def populate_matrix_smart(matrix, horizontal, vertical):
 def create_image(matrix, initial_data):
     """
     Creates 2D image based on data points in matrix
+
     Parameters
     ----------
     matrix : 2D array 
         array that contains data for image
-    
+    initial_data : Dictionary
+        contains paramter data from Dat file
     Notes
     -----
     
     """
     plt.figure(1)
-    plt.subplot(211)
-    im = plt.imshow(matrix, aspect=2,
+    ax1 = plt.subplot2grid((3, 3), (0, 0), colspan=2, rowspan=2)
+    ax2 = plt.subplot2grid((3, 3), (2, 0), colspan=2)
+    ax3 = plt.subplot2grid((3, 3), (0, 2), rowspan=2)
+
+    horizontal_initial = _unitChange(initial_data['horizontal']['initial_position'], 'micro')
+    horizontal_final = _unitChange(initial_data['horizontal']['final_position'], 'micro')
+    vertical_initial = _unitChange(initial_data['vertical']['initial_position'], 'micro')
+    vertical_final = _unitChange(initial_data['vertical']['final_position'], 'micro')
+
+    ax1.imshow(matrix, aspect = 1.5,
                     extent=(
-                        initial_data['horizontal']['initial_position']*1e6, initial_data['horizontal']['final_position']*1e6,
-                        initial_data['vertical']['initial_position']*1e6, initial_data['vertical']['final_position']*1e6
-                    ))
-    # plt.colorbar(im, orientation='vertical')
-    plt.xlabel('Horizontal Position [µm]')
-    plt.ylabel('Vertical Position [µm]')
-    plt.title(f'Photon energy: {initial_data["photon"]["final_energy"]} eV')
+                         horizontal_initial, horizontal_final,
+                         vertical_initial, vertical_final)
+                    )
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    ax1.set_title(f'After Propagation (E= {initial_data["photon"]["final_energy"]} {initial_data["photon"]["units"]})')
+
+    ax2.plot(matrix[0])
+
+    vertical = []
+    for i in matrix:
+        vertical.append(i[0])
+    ax3.plot(vertical)
     plt.show()
+
+
+
+
+def _unitChange(value_to_be_changed, unit):
+    """
+    Changes value based on given unit
+
+    Parameters
+    ----------
+    value_to_be_changed : float
+        value that is returned once it is changed
+    unit : string
+        unit the value should be converted to
+
+    """
+    if unit is 'micro':
+        return value_to_be_changed * 1e6
+    elif unit is 'nano':
+        return value_to_be_changed * 1e9
 
 
 if __name__ == '__main__':
@@ -174,7 +215,7 @@ if __name__ == '__main__':
     horizontal = initial_data['horizontal']['points']
     vertical = initial_data['vertical']['points']
 
-    #matrix = populate_matrix_smart(matrix, horizontal, vertical)
+    matrix = populate_matrix_smart(matrix, horizontal, vertical)
     matrix = populate_matrix(matrix, horizontal, vertical)
     print(initial_data)
     create_image(matrix, initial_data)
